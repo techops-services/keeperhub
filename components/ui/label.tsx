0320aff247b8399fb0cd8181c4fdaa4d2e1df814
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-04 14:16:21 -0800

    Add all shadcn components

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,19 +1,24 @@
-import * as React from 'react';
+"use client"
 
-import { cn } from '@/lib/utils';
+import * as React from "react"
+import { Label as LabelPrimitive } from "radix-ui"
 
-const Label = React.forwardRef<HTMLLabelElement, React.ComponentProps<'label'>>(
-  ({ className, ...props }, ref) => (
-    <label
-      ref={ref}
+import { cn } from "@/lib/utils"
+
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
       className={cn(
-        'text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
       {...props}
     />
   )
-);
-Label.displayName = 'Label';
+}
 
-export { Label };
+export { Label }
