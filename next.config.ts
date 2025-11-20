0600eece1fd41aecf40e18f0c005b9162f16ca50
Chris Tate <chris@ctate.dev>
2025-11-20 00:46:56 -0600

    format

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import { withWorkflow } from 'workflow/next'; 
 import type { NextConfig } from "next";
+import { withWorkflow } from "workflow/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withWorkflow(nextConfig); 
+export default withWorkflow(nextConfig);
