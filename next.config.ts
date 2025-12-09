063141d51c0d7cbde5f3dea6267567fd23ded4df
taitsengstock <taitsengstock@gmail.com>
2025-12-09 12:01:05 +1100

    Don't save runs to disk

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
 };
 
-export default withWorkflow(nextConfig);
+export default withWorkflow(nextConfig, {
+  // Disable file-based storage since we use database logging
+  storage: { type: "memory" },
+});
