9706aa7fcade78335c5f19e39be0983dd0406683
Jacob Sussmilch <suisuss@protonmail.com>
2026-02-03 15:10:39 +1100

    fix: KEEP-491 Regenerate migration with drizzle-kit journal

--- drizzle/0016_user-deactivated-at.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add deactivatedAt column to users table
--- Ticket: KEEP-491
--- Description: Soft delete support for user account deactivation
-
-ALTER TABLE "users" ADD COLUMN "deactivated_at" timestamp;
